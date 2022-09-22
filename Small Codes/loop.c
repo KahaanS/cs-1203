38dd920d2f1193939a7181fce00accc3b1c09c2b
Kahaan Shah <Kahaan@Kahaans-MacBook-Pro.local>
2022-09-22 14:05:05 +0530

    Loop tester code

---
+++ Small Codes/loop.c
@@ -0,0 +1,13 @@
+//
+// Created by Kahaan Shah on 19/09/22.
+//
+
+#include <stdio.h>
+
+int main() {
+    for (float a=0.0; a<0.000000000000001; a=a+0.0000000000000001) {
+        printf("hello \n");
+    }
+
+    return 0;
+}
